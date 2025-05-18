c21d054624bc085282c8691b343e2bc04be42bce
Eshel Yaron <me@eshelyaron.com>
2025-05-18 12:16:42 +0200

    Prove OBdd.numPointers_le

--- Bdd/Basic.lean
+++ Bdd/Basic.lean
@@ -2627,3 +2627,99 @@ instance OBdd.instDecidableUsesVar {O : OBdd n m} : DecidablePred O.1.usesVar :=
   intro i
   simp [usesVar]
   infer_instance
+
+lemma OBdd.numPointers_node_le {O : OBdd n m} {h : O.1.root = node j} :
+    O.numPointers ≤ 1 + (O.low h).numPointers + (O.high h).numPointers := by
+  rw [numPointers_spec]
+  rw [numPointers_spec]
+  rw [numPointers_spec]
+  rw [show 1 = Fintype.card {j' // j' = j} by simp]
+  rw [add_assoc]
+  rw [← Fintype.card_sum]
+  rw [← Fintype.card_sum]
+  refine Fintype.card_le_of_embedding ⟨?_, ?_⟩
+  · exact
+    fun ⟨j', hj'⟩ ↦ match decEq j' j with
+      | isTrue ht => .inl ⟨j', ht⟩
+      | isFalse hf => .inr (
+        match instDecidableReachable (O.low h) (node j') with
+        | isTrue htt => .inl ⟨j', htt⟩
+        | isFalse hff => .inr ⟨j', by
+          apply reachable_or_eq_low_high at hj'
+          cases hj' with
+          | inl hh => simp_all
+          | inr hh =>
+            rcases hh with ⟨jj, hjj1, hjj2⟩
+            have : node j = node jj := by rw [← h, hjj1]
+            injection this with that
+            subst that
+            cases hjj2 with
+            | inl hhh => simp_all
+            | inr hhh => exact hhh
+          ⟩
+        )
+  · intro x y hxy
+    simp only [low_heap_eq_heap, high_heap_eq_heap] at hxy
+    split at hxy
+    next =>
+      split at hxy <;> (refine Subtype.eq ?_; simp_all)
+    next =>
+      split at hxy <;>
+      (next =>
+        split at hxy
+        next => simp_all
+        next => split at hxy <;> (refine Subtype.eq ?_; simp_all))
+
+lemma OBdd.numPointers_le' {O : OBdd n m} : O.numPointers ≤ 2 ^ (n - O.1.var.1) - 1 := by
+  cases O_root_def : O.1.root with
+  | terminal b => simp [isTerminal_iff_numPointer_eq_zero.mpr ⟨b, O_root_def⟩]
+  | node j =>
+    calc _
+      _ ≤ 1 + (O.low O_root_def).numPointers + (O.high O_root_def).numPointers := numPointers_node_le
+      _ ≤ 1 + (2 ^ (n - (O.low O_root_def).1.var.1) - 1) + (2 ^ (n - (O.high O_root_def).1.var.1) - 1) := by
+        have := numPointers_le' (O := O.low O_root_def)
+        have := numPointers_le' (O := O.high O_root_def)
+        omega
+      _ ≤ 1 + (2 ^ (n - (O.1.var.1 + 1)) - 1) + (2 ^ (n - (O.high O_root_def).1.var.1) - 1) := by
+        simp only [Nat.succ_eq_add_one, low_heap_eq_heap, add_le_add_iff_right, add_le_add_iff_left, tsub_le_iff_right]
+        rw [Nat.sub_add_cancel (by exact Nat.one_le_two_pow)]
+        apply pow_le_pow_right' (by simp)
+        have := var_lt_low_var (h := O_root_def)
+        simp only [var] at this
+        omega
+      _ ≤ 1 + (2 ^ (n - (O.1.var.1 + 1)) - 1) + (2 ^ (n - (O.1.var.1 + 1)) - 1) := by
+        simp only [Nat.succ_eq_add_one, low_heap_eq_heap, add_le_add_iff_right, add_le_add_iff_left, tsub_le_iff_right]
+        rw [Nat.sub_add_cancel (by exact Nat.one_le_two_pow)]
+        apply pow_le_pow_right' (by simp)
+        have := var_lt_high_var (h := O_root_def)
+        simp only [var] at this
+        omega
+      _ ≤ 1 + (2 ^ ((n - O.1.var.1) - 1) - 1) + (2 ^ (n - (O.1.var.1 + 1)) - 1) := by
+        simp only [Nat.succ_eq_add_one, add_le_add_iff_right, add_le_add_iff_left, tsub_le_iff_right]
+        rw [Nat.sub_add_cancel (by exact Nat.one_le_two_pow)]
+        apply pow_le_pow_right' (by simp)
+        omega
+      _ ≤ 1 + (2 ^ ((n - O.1.var.1) - 1) - 1) + (2 ^ ((n - O.1.var.1) - 1) - 1) := by
+        simp only [Nat.succ_eq_add_one, add_le_add_iff_right, add_le_add_iff_left, tsub_le_iff_right]
+        rw [Nat.sub_add_cancel (by exact Nat.one_le_two_pow)]
+        apply pow_le_pow_right' (by simp)
+        omega
+      _ ≤ 2 ^ ((n - O.1.var.1) - 1) + 2 ^ ((n - O.1.var.1) - 1) - 1 := by
+        simp only [Nat.succ_eq_add_one]
+        have := Nat.one_le_two_pow (n := ((n - O.1.var.1) - 1))
+        omega
+      _ ≤ 2*2 ^ ((n - O.1.var.1) - 1) - 1 := by omega
+      _ ≤ 2 ^ (n - O.1.var.1) - 1 := by
+        refine Nat.sub_le_sub_right ?_ 1
+        apply le_of_eq
+        refine mul_pow_sub_one ?_ 2
+        simp [O_root_def]
+        omega
+termination_by O
+
+lemma OBdd.numPointers_le {O : OBdd n m} : O.numPointers ≤ 2 ^ n - 1 := by
+  trans 2 ^ (n - O.1.var.1) - 1
+  · exact OBdd.numPointers_le'
+  · simp only [Nat.succ_eq_add_one, tsub_le_iff_right]
+    rw [Nat.sub_add_cancel (by exact Nat.one_le_two_pow)]
+    apply Nat.pow_le_pow_right <;> omega
