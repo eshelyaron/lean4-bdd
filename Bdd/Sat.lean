4ebb2965684ac35ab9e18bf77e5f2383e1b62161
Eshel Yaron <me@eshelyaron.com>
2025-09-02 10:56:31 +0200

    Sat.lean: Remove a few more unused simp arguments

--- Bdd/Sat.lean
+++ Bdd/Sat.lean
@@ -28,15 +28,15 @@ lemma BDD_of_CNF_correct {n} {f : Fin n → Bool} (C : Std.Sat.CNF (Fin n)) :
   | nil => simp [BDD_of_CNF]
   | cons head tail ih =>
     simp only [Std.Sat.CNF.eval_cons, BDD_of_CNF, List.map_cons, List.foldr_cons, BDD.and_denotation]
-    simp only [Std.Sat.CNF.eval_cons, BDD_of_CNF, List.map_cons, List.foldr_cons] at ih
+    simp only [BDD_of_CNF] at ih
     rw [ih]
     congr 1
     induction head with
     | nil => simp [BDD_of_clause]
     | cons head tail ih =>
-      simp only [Std.Sat.CNF.Clause.eval_cons, Fin.eta]
+      simp only [Std.Sat.CNF.Clause.eval_cons]
       rw [ih]
-      simp only [BDD_of_clause, Fin.eta, List.map_cons, List.foldr_cons, BDD.or_denotation]
+      simp only [BDD_of_clause, List.map_cons, List.foldr_cons, BDD.or_denotation]
       congr 1
       simp only [BDD_of_literal]
       split <;> simp_all
@@ -50,7 +50,7 @@ instance instDecidableUnsat (C : Std.Sat.CNF (Fin n)) : Decidable (Std.Sat.CNF.U
       simp only [Std.Sat.CNF.Unsat, not_forall, Bool.not_eq_false] at h
       rcases h with ⟨f, hf⟩
       rw [BDD_of_CNF_correct] at hf
-      simp only [Fin.eta, BDD.const_nvars, BDD.const_denotation, Function.const_apply, not_forall, Bool.not_eq_false]
+      simp only [BDD.const_nvars, BDD.const_denotation, Function.const_apply, not_forall, Bool.not_eq_false]
       use Vector.cast (by simp) (Vector.ofFn fun i : Fin (BDD_of_CNF C).nvars ↦ f ⟨i.1, lt_of_lt_of_le i.2 BDD_of_CNF_nvars⟩)
       simp only [le_refl, BDD.denotation_cast]
       rw [BDD.denotation_take'] at hf
